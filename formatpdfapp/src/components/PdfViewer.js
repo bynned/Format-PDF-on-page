f52359e109796d247121c917fb2fb0bab9f6ad6f
bynned <benny.cascarino@tuni.fi>
2023-06-05 10:49:28 +0300

    Such wow

---
+++ formatpdfapp/src/components/PdfViewer.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const PdfViewer = () => {
+  const [pdfUrl, setPdfUrl] = useState('');
+
+  const handlePdfUpload = (event) => {
+    const file = event.target.files[0];
+    const fileUrl = URL.createObjectURL(file);
+    setPdfUrl(fileUrl);
+  };
+
+  return (
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <input type="file" accept="application/pdf" onChange={handlePdfUpload} />
+      {pdfUrl && (
+        <iframe
+          src={pdfUrl}
+          title="PDF Viewer"
+          style={{ flex: 1, border: 'none' }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PdfViewer;
